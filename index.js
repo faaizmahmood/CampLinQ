93c605454c1973b88615fcc06f3a912ee1616a41
faaiz mahmood <faizzafar44@gmail.com>
2024-01-10 13:30:23 +0500

    95% compete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // --------------swap function
 
 function swap_content() {
-    console.log("ghghh");
+
 
     let source = document.querySelector('#source');
     let destination = document.querySelector('#destination');
@@ -24,9 +24,8 @@ function swap_content() {
 function add_new_fields(){
 
     let new_fields = document.querySelector("#new-fields");
-
     new_fields.style.display = "block"
 
-
-
 }
+
+
